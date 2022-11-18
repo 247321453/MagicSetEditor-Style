6e502e483093c9937decc4d9c5ed4e3d93f68355
kenan <247321453@qq.com>
2022-11-18 11:01:06 +0800

    update mse

--- fix don't open.bat
+++ fix don't open.bat
@@ -1,8 +1,10 @@
 @echo off
 echo.
 set config=%appdata%\Magic Set Editor\mse8.config
+set config2=%appdata%\Magic Set Editor\mse.config
 echo ½«É¾³ýÎÄ¼þ %config%
 echo.
 pause
 del /q "%config%"
+del /q "%config2%"
 pause
